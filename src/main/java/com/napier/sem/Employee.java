5a476e98413d20a1bb3bbbfbefcfb6bb7e9daf76
40322938 <killeroid356@gmail.com>
2019-01-30 12:07:00 +0000

    Finaly Worked

--- src/main/java/com/napier/sem/Employee.java
+++ src/main/java/com/napier/sem/Employee.java
@@ -40,39 +40,4 @@ public class Employee
      */
     public String manager;
 
-
-    /** these lines of code cause huge errors and i still dont know why any of this isnt working i followed all the steps in the lab, why :(
-     * public Employee getEmployee(int ID)
-    {
-        try
-        {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
-            if (rset.next())
-            {
-                Employee emp = new Employee();
-                emp.emp_no = rset.getInt("emp_no");
-                emp.first_name = rset.getString("first_name");
-                emp.last_name = rset.getString("last_name");
-                return emp;
-            }
-            else
-                return null;
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
-            return null;
-        }
-    }*/
 }
